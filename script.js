05be1e215d6231608c074e6a62a478014c5e068d
Stephens37 <braydenstephens@protonmail.com>
2023-06-02 13:39:14 -0500

    "Create DOM elements for card"

--- script.js
+++ script.js
@@ -9,18 +9,34 @@ function Book (title = 'unknown', pages = 'unknown') {
   }
 }
 
+const bookDiv = document.createElement('div')
+bookDiv.classList.add('bookcard')
+const bookTitle = document.createElement('div')
+bookTitle.classList.add('title')
+bookDiv.appendChild(bookTitle)
+const bookPages = document.createElement('div')
+bookPages.classList.add('pages')
+bookDiv.appendChild(bookPages)
+
 
 function addToLibrary () {
-  const player1 = new Book(document.getElementById('title').value, document.getElementById('pages').value)
+  const htmlTitle = document.getElementById('title').value
+  const htmlPages = document.getElementById('pages').value
+  const player1 = new Book(htmlTitle, htmlPages)
   player1.sayName()
-  /*const bookDiv = document.createElement('div')
+
+  const bookDiv = document.createElement('div')
   bookDiv.classList.add('bookcard')
+
   const bookTitle = document.createElement('div')
-  bookTitle.classList.add('title')
-  bookDiv.appendChild('title')
+  bookTitle.classList.add('cardtitle')
+  bookTitle.innerHTML = htmlTitle
+  bookDiv.appendChild(bookTitle)
   const bookPages = document.createElement('div')
-  bookPages.classList.add('pages')
-  bookDiv.appendChild('pages')*/
+  bookPages.classList.add('cardpages')
+  bookPages.innerHTML = htmlPages
+  bookDiv.appendChild(bookPages)
+
   document.getElementById('book_info').reset()
 }
 
