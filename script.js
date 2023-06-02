060cbbbb0235c86ad6bd9dd6a3a945ceb6b381a3
Stephens37 <braydenstephens@protonmail.com>
2023-06-02 12:44:24 -0500

    "Work on adding array data to card"

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const myLibrary = []
 
-function Book(title = 'unknown', pages = 'unknown') {
+function Book (title = 'unknown', pages = 'unknown') {
   this.title = title
   this.pages = pages
   this.sayName = function () {
@@ -13,5 +13,14 @@ function Book(title = 'unknown', pages = 'unknown') {
 function addToLibrary () {
   const player1 = new Book(document.getElementById('title').value, document.getElementById('pages').value)
   player1.sayName()
+  /*const bookDiv = document.createElement('div')
+  bookDiv.classList.add('bookcard')
+  const bookTitle = document.createElement('div')
+  bookTitle.classList.add('title')
+  bookDiv.appendChild('title')
+  const bookPages = document.createElement('div')
+  bookPages.classList.add('pages')
+  bookDiv.appendChild('pages')*/
+  document.getElementById('book_info').reset()
 }
 
